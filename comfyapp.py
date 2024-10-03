ec0b8888ce663896941789b1509cdb0a532fde3b
ธนบดี นามมุงคุณ <original2547@gmail.com>
2024-10-04 03:22:44 +0700

    First Version

---
+++ comfyapp.py
@@ -0,0 +1,201 @@
+# ---
+# cmd: ["modal", "serve", "06_gpu_and_ml/comfyui/comfyapp.py"]
+# deploy: true
+# ---
+#
+# # Run Flux on ComfyUI interactively and as an API
+#
+# [ComfyUI](https://github.com/comfyanonymous/ComfyUI) is an open-source Stable Diffusion GUI with a graph/nodes based interface that allows you to design and execute advanced image generation pipelines.
+
+# Flux is a family of cutting-edge text-to-image models created by [black forest labs](https://huggingface.co/black-forest-labs), rapidly gaining popularity due to their exceptional image quality.
+#
+# In this example, we show you how to
+#
+# 1. run Flux on ComfyUI interactively to develop workflows
+#
+# 2. serve a Flux ComfyUI workflow as an API
+#
+# Combining the UI and the API in a single app makes it easy to iterate on your workflow even after deployment.
+# Simply head to the interactive UI, make your changes, export the JSON, and redeploy the app.
+#
+# ## Quickstart
+#
+# This example runs `workflow_api.json` in this directory, which is an adapation of [this simple FLUX.1-schnell workflow](https://openart.ai/workflows/reverentelusarca/flux-simple-workflow-schnell/40OkdaB23J2TMTXHmxxu) with an Image Resize custom node added at the end.
+#
+# For the prompt `"Surreal dreamscape with floating islands, upside-down waterfalls, and impossible geometric structures, all bathed in a soft, ethereal light"`
+# we got this output:
+#
+# ![example comfyui image](./flux_gen_image.jpeg)
+#
+# To serve the workflow in this example as an API:
+# 1. Stand up the ComfyUI server in development mode:
+# ```bash
+# modal serve 06_gpu_and_ml/comfyui/comfyapp.py
+# ```
+# Note: if you're running this for the first time, it will take several minutes to build the image, since we have to download the Flux models (>20GB) to the container. Successive calls will reuse this prebuilt image.
+#
+# 2. In another terminal, run inference:
+# ```bash
+# python 06_gpu_and_ml/comfyui/comfyclient.py --dev --modal-workspace $(modal profile current) --prompt "neon green sign that says Modal"
+# ```
+#
+# The first inference will take ~1m since the container needs to launch the ComfyUI server and load Flux into memory. Successive inferences on a warm container should take a few seconds.
+#
+# ## Setup
+#
+# First, we define the environment we need to run ComfyUI using [`comfy-cli`](https://github.com/Comfy-Org/comfy-cli). This handy tool manages the installation of ComfyUI, its dependencies, models, and custom nodes.
+
+
+import json
+import subprocess
+from pathlib import Path
+from typing import Dict
+import base64
+
+import modal
+
+image = (  # build up a Modal Image to run ComfyUI, step by step
+    modal.Image.debian_slim(  # start from basic Linux with Python
+        python_version="3.11"
+    )
+    .apt_install("git")  # install git to clone ComfyUI
+    .pip_install("comfy-cli==1.2.4")  # install comfy-cli
+    .run_commands(  # use comfy-cli to install the ComfyUI repo and its dependencies
+        "comfy --skip-prompt install --nvidia"
+    )
+    .run_commands(  # download the flux models
+        "comfy --skip-prompt model download --url https://huggingface.co/a34384300/XSarchitectural-InteriorDesign-ForXSLora/resolve/main/xsarchitectural_v11.ckpt --relative-path models/checkpoints",
+    )
+    .run_commands(  # download a custom node
+        "comfy node install Comfy-Photoshop-SD"
+    )
+    # can layer additional models and custom node downloads as needed
+)
+
+app = modal.App(name="exep-comfyui", image=image)
+
+
+# ## Running ComfyUI interactively and as an API on Modal
+#
+# To run ComfyUI interactively, simply wrap the `comfy launch` command in a Modal Function and serve it as a web server.
+@app.function(
+    allow_concurrent_inputs=10,
+    concurrency_limit=1,
+    container_idle_timeout=30,
+    timeout=1800,
+    gpu="L4",
+)
+@modal.web_server(8000, startup_timeout=60)
+def ui():
+    subprocess.Popen("comfy launch -- --listen 0.0.0.0 --port 8000", shell=True)
+
+
+# Remember to **close your UI tab** when you are done developing to avoid accidental charges to your account.
+# This will close the connection with the container serving ComfyUI, which will spin down based on your `container_idle_timeout` setting.
+#
+# To run an existing workflow as an API, we use Modal's class syntax to run our customized ComfyUI environment and workflow on Modal.
+#
+# Here's the basic breakdown of how we do it:
+# 1. We stand up a "headless" ComfyUI server in the background when the app starts.
+# 2. We define an `infer` method that takes in a workflow path and runs the workflow on the ComfyUI server.
+# 3. We stand up an `api` with `web_endpoint`, so that we can run our workflows as a service.
+#
+# For more on how to run web services on Modal, check out [this guide](https://modal.com/docs/guide/webhooks).
+@app.cls(
+    allow_concurrent_inputs=10,
+    container_idle_timeout=300,
+    gpu="L4",
+    mounts=[
+        modal.Mount.from_local_file(
+            Path(__file__).parent / "img2img_comfy.json",
+            "/root/img2img_comfy.json",
+        ),
+    ],
+)
+class ComfyUI:
+    @modal.enter()
+    def launch_comfy_background(self):
+        cmd = "comfy launch --background"
+        subprocess.run(cmd, shell=True, check=True)
+
+    @modal.method()
+    def infer(self, workflow_path: str = "/root/img2img_comfy.json"):
+        # runs the comfy run --workflow command as a subprocess
+        cmd = f"comfy run --workflow {workflow_path} --wait --timeout 1200"
+        print(f"Running command: {cmd}")
+        subprocess.run(cmd, shell=True, check=True)
+
+        # completed workflows write output images to this directory
+        output_dir = "/root/comfy/ComfyUI/output"
+        print(f"Output images saved to {output_dir}")
+        # looks up the name of the output image file based on the workflow
+        workflow = json.loads(Path(workflow_path).read_text())
+        file_prefix = [
+            node.get("inputs")
+            for node in workflow.values()
+            if node.get("class_type") == "SaveImage"
+        ][0]["filename_prefix"]
+        images_list= []
+        # returns the image as bytes
+        for f in Path(output_dir).iterdir():
+            if f.name.startswith(file_prefix):
+                images_list.append(f)
+        
+        # Ensure that the list is not empty before attempting to read bytes
+        if images_list:
+            return [base64.b64encode(f.read_bytes()).decode('utf-8') for f in images_list]
+        else:
+            raise FileNotFoundError(f"No output images found with prefix {file_prefix}")
+
+    @modal.web_endpoint(method="POST")
+    def api(self, item: Dict):
+        from fastapi import Response
+        import random
+
+        workflow_data = json.loads(
+            (Path(__file__).parent / "img2img_comfy.json").read_text()
+        )
+
+        # insert the prompt
+
+        workflow_data["6"]["inputs"]["text"] = item["pos_prompt"]
+        workflow_data["7"]["inputs"]["text"] = item["neg_prompt"]
+        workflow_data["3"]["inputs"]["seed"] = random.randint(0, 99999999999999)
+        workflow_data["17"]["inputs"]["image_base64"] = item["image_data"]
+        workflow_data["3"]["inputs"]["denoise"] = item["effect_rate"]
+        
+        # give the output image a unique id per client request
+        client_id = item["image_name"]
+        workflow_data["9"]["inputs"]["filename_prefix"] = client_id
+
+        # save this updated workflow to a new file
+        new_workflow_file = f"{client_id}.json"
+        # json.dump(workflow_data, Path(new_workflow_file).open("w"))
+        with open(new_workflow_file, 'w') as f:
+            json.dump(workflow_data, f)
+
+        # run inference on the currently running container
+        img_base_64 = self.infer.local(f"/root/{new_workflow_file}")
+
+        
+        return Response(content=json.dumps(img_base_64), status_code=200)
+
+
+# ### The workflow for developing workflows
+#
+# When you run this script with `modal deploy 06_gpu_and_ml/comfyui/comfyapp.py`, you'll see a link that includes `ui`.
+# Head there to interactively develop your ComfyUI workflow. All of your models and custom nodes specified in the image build step will be loaded in.
+#
+# To serve the workflow after you've developed it, first export it as "API Format" JSON:
+# 1. Click the gear icon in the top-right corner of the menu
+# 2. Select "Enable Dev mode Options"
+# 3. Go back to the menu and select "Save (API Format)"
+#
+# Save the exported JSON to the `workflow_api.json` file in this directory.
+#
+# Then, redeploy the app with this new workflow by running `modal deploy 06_gpu_and_ml/comfyui/comfyapp.py` again.
+#
+# ## Further optimizations
+# - To decrease inference latency, you can process multiple inputs in parallel by setting `allow_concurrent_inputs=1`, which will run each input on its own container. This will reduce overall response time, but will cost you more money. See our [Scaling ComfyUI](https://modal.com/blog/scaling-comfyui) blog post for more details.
+# - If you're noticing long startup times for the ComfyUI server (e.g. >30s), this is likely due to too many custom nodes being loaded in. Consider breaking out your deployments into one App per unique combination of models and custom nodes.
+# - For those who prefer to run a ComfyUI workflow directly as a Python script, see [this blog post](https://modal.com/blog/comfyui-prototype-to-production).
---
+++ comfyclient.py
@@ -0,0 +1,119 @@
+# ---
+# cmd: ["python", "06_gpu_and_ml/comfyui/comfyclient.py", "--modal-workspace", "modal-labs", "--prompt", "Spider-Man visits Yosemite, rendered by Blender, trending on artstation"]
+# output-directory: "/tmp/comfyui"
+# ---
+
+import pathlib
+import sys
+import json
+import time
+import os
+import requests
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+import uvicorn
+import aiohttp
+
+OUTPUT_DIR = pathlib.Path("/tmp/comfyui")
+OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
+
+
+
+model_workspace = "thanabordeen"
+config_path = "./prompt.json"
+
+OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://localhost:11434")
+URL = f"{OLLAMA_HOST}/api/generate"
+HEADERS = {'Content-type': 'application/json'}
+with open(config_path, encoding="UTF-8") as f:
+    DEFAULT_PROMPT = json.load(f)
+
+# --- FastAPI App ---
+app = FastAPI(
+    title="Refine Image",
+    description="Generate a Stable Diffusion prompt based on user input and refine image.",
+    version="1.0.0",
+    docs_url="/docs",
+)
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+    
+class Receive(BaseModel):
+    prompt: str
+    image_base64: str
+    img_name: str
+    
+@app.post("/generate-prompt")
+async def generate_prompt(receive: Receive):
+
+    prompt_data = DEFAULT_PROMPT.copy() 
+    prompt_data["prompt"] = receive.prompt
+
+    try:
+        response = await make_post_request(URL, prompt_data, HEADERS)
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Error communicating with Ollama: {e}")
+
+    prompt_info = extract_prompt_and_negative_prompt(json.loads(response)['response'])
+    pos_prompt = prompt_info["prompt"]
+    neg_prompt = prompt_info["negative_prompt"]
+
+    images_list = await generate_image(pos_prompt, neg_prompt, receive.image_base64, 0.5, receive.img_name)
+    data ={"images": images_list}
+        
+    return data
+
+    
+    
+    
+async def generate_image(pos_prompt: str, neg_prompt: str, image: str, effect_rate: float, image_name: str, dev: bool = True):
+
+    url = f"https://{model_workspace}--exep-comfyui-comfyui-api{'-dev' if dev else ''}.modal.run/"
+    data = {
+        "pos_prompt": pos_prompt,
+        "neg_prompt": neg_prompt,
+        "image_data": image,
+        "effect_rate": effect_rate,
+        "image_name": image_name
+    }
+    print(f"Sending request to {url} with prompt: {data['pos_prompt']}")
+    print("Waiting for response...")
+    start_time = time.time()
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=data) as res:
+            print(f"Response received in {time.time() - start_time} seconds.")
+            if res.status != 200:
+                raise HTTPException(status_code=res.status, detail=await res.text())
+            try:
+                images = await res.content.read()
+                images = json.loads(images) # This make it's slow to load
+            except aiohttp.ContentTypeError as e:
+                raise HTTPException(status_code=500, detail=f"Error parsing response JSON: {e}")
+            return images
+            
+        
+async def make_post_request(url, prompt, headers):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=json.dumps(prompt), headers=headers) as response:
+            return await response.text()
+        
+def extract_prompt_and_negative_prompt(response: str):
+    import re
+    prompt_match = re.search(r'<Prompt>(.*?)</Prompt>', response, re.DOTALL)
+    negative_prompt_match = re.search(r'<Negative Prompt>(.*?)</Negative Prompt>', response, re.DOTALL)
+    return {
+        "prompt": prompt_match.group(1).strip() if prompt_match else None,
+        "negative_prompt": negative_prompt_match.group(1).strip() if negative_prompt_match else None,
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
